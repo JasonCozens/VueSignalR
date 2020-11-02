c44deb81dc6355b6ac02095d0bd935cf910ad43c
Edward Jason Cozens <EdwardJason_Cozens@next.co.uk>
2020-11-02 20:35:16 +0000

    Outline of board from specflow input.

---
+++ Games/Waiting4Rain.Games.TileGameSF/Features/TileGame.feature
@@ -0,0 +1,13 @@
+﻿Feature: TileGame
+
+Scenario: Final Position
+	Given the final position is:
+	| ROW	   |
+	| T1 T2 T3 |
+	| T4 T5 T6 |
+	| T7 T8 __ |
+	Then the board is:
+	| ROW	   |
+	| T1 T2 T3 |
+	| T4 T5 T6 |
+	| T7 T8 T9 |
---
+++ Games/Waiting4Rain.Games.TileGameSF/Steps/TileGameSteps.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using TechTalk.SpecFlow;
+
+namespace Waiting4Rain.Games.TileGameSF.Steps
+{
+    internal class TileGameContext
+    {
+
+    }
+
+
+    [Binding]
+    public class TileGameSteps
+    {
+
+        [Given(@"the final position is:")]
+        public void GivenTheFinalPositionIs(Table table)
+        {
+            var board = new List<List<string>>();
+            for (int r = 0; r < table.Rows.Count; r++)
+            {
+                var row = table.Rows[r].Values.First();
+                board.Add(row.Trim().Split(" ").ToList());
+            }
+            Debug.WriteLine("STOP");
+        }
+        
+        [Then(@"the board is:")]
+        public void ThenTheBoardIs(Table table)
+        {
+        }
+    }
+}
